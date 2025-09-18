e63210c50426eaf2182a869bcee8e3649b6c3d87
Diptishrivas <Shrivasdipti85@gmail.com>
2025-09-19 00:28:30 +0530

    (19-09 )Funtions (reducing duplicates and splitinh complex task)

---
+++ 05_functions/02_complex.py
@@ -0,0 +1,17 @@
+def fetch_sales():
+    print("Fecthing the sale data")
+
+
+def filter_valid_sales():
+    print(" filter sales data")
+
+def summaries_data():
+    print("Summarize sales data")
+
+def generate_report():
+    fetch_sales()
+    filter_valid_sales()
+    summaries_data()
+    print("Report is ready")
+
+generate_report()
