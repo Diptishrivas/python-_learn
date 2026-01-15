a15f50578d279b4479a055a5a3b5c56b820bbe98
Diptishrivas <Shrivasdipti85@gmail.com>
2026-01-15 21:15:50 +0530

    oops

--- 10_oops/class_object.py
+++ 10_oops/class_object.py
@@ -17,3 +17,14 @@ masala.is_hot= False
 
 print(f"Class:{Chai.is_hot}")
 print(f"Masala{masala.is_hot}")
+
+
+
+# Percentage Calculator
+
+total_marks = float(input("Enter total marks: "))
+obtained_marks = float(input("Enter obtained marks: "))
+
+percentage = (obtained_marks / total_marks) * 100
+
+print("Your Percentage is:", round(percentage, 2), "%")
